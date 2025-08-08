1605b9ea460c0531a9c4d76aee5ec598a36f0f34
MevanManilka <w.a.mevanmanilka@gmail.com>
2025-08-08 21:50:51 +0530

    fix: update imports after Thumbnail* renames in shared components

--- features/thumbnail/common/ImageDropZone.tsx
+++ features/thumbnail/common/ImageDropZone.tsx
@@ -25,7 +25,7 @@ import {
 
 import { Slider } from "@/components/ui/slider"
 import { Label } from "@/components/ui/label"
-import TextEditor from "@/features/thumbnail/common/TextEditor"
+import TextEditor from "@/features/thumbnail/common/ThumbnailTextEditor"
 
 interface ImageInfo {
   width: number
--- features/thumbnail/common/ThumbnailTextEditorSection.tsx
+++ features/thumbnail/common/ThumbnailTextEditorSection.tsx
@@ -2,7 +2,7 @@
 
 import React from "react"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import TextEditor from "@/features/thumbnail/common/TextEditor"
+import TextEditor from "@/features/thumbnail/common/ThumbnailTextEditor"
 import { AlertCircle } from "lucide-react"
 
 interface SharedTextEditorSectionProps {
